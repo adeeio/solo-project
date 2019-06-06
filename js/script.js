9cf13e8186c91935bd1d605c258c8bf37f1b165b
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-06-06 20:38:31 +0100

    finished navbar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,2 @@
 "use strict";
 
-let mainNav = document.getElementById("js-menu");
-
-let navBarToggle = document.getElementById("js-navbar-toggle");
-
-navBarToggle.addEventListener("click", function() {
-    mainNav.classList.toggle("active");
-});
