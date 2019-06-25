b7a46d26c62a69e10c9d9d7558adc6d2a6e48bdc
Adrian Ostapkiewicz <ostapkiewicz.adrian@gmail.com>
2019-06-25 22:06:26 +0100

    installed images compress package and included it into the task runner

---
+++ compress.js
@@ -0,0 +1,16 @@
+var compress_images = require('compress-images'), input, output;
+ 
+    input = 'images/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+    output = 'output/';
+    
+    compress_images(input, output, {compress_force: false, statistic: true, autoupdate: true}, false,
+                                                {jpg: {engine: 'mozjpeg', command: ['-quality', '60']}},
+                                                {png: {engine: 'pngquant', command: ['--quality=20-50']}},
+                                                {svg: {engine: 'svgo', command: '--multipass'}},
+                                                {gif: {engine: 'gifsicle', command: ['--colors', '64', '--use-col=web']}}, function(error, completed, statistic){
+                console.log('-------------');
+                console.log(error);
+                console.log(completed);
+                console.log(statistic);
+                console.log('-------------');                                   
+    });
